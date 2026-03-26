1fb3d23315f502551405b8d7299e174e397e171e
jadeprideaux <jadeprideaux7@gmail.com>
2026-03-26 12:25:53 +0000

    refactor: updated interface to use blocks format

--- app.py
+++ app.py
@@ -18,18 +18,54 @@ def midi_file_to_jianpu_str(
         return f"Error processing file, make sure to upload a MIDI file."
     return midi_to_jianpu_str(midi, channel, semitone_offset, octave_offset, time_interval)
 
-interface = gr.Interface(
-    fn = midi_file_to_jianpu_str,
-    inputs = [
-        gr.File(label="Upload MIDI file"),
-        gr.Slider(minimum=0, maximum=15, step=1, value=0, label="MIDI Channel"),
-        gr.Slider(minimum=-11, maximum=11, step=1, value=0, label="Semitone Offset"),
-        gr.Slider(minimum=-8, maximum=8, step=1, value=0, label="Octave Offset"),
-        gr.Slider(minimum=1, maximum=16, step=1, value=8, label="Time Interval")
-    ],
-    outputs = "text",
-    title = "MIDI to Jianpu"
-)
+with gr.Blocks() as ui:
+
+    with gr.Row():
+        with gr.Column():
+            gr.Markdown("# MIDI to Jianpu")
+            gr.Markdown("## How to use:")
+            gr.Markdown("## Key:")
+
+        with gr.Column():
+            midi_input = gr.File(label="Upload MIDI file")
+
+            channel_slider = gr.Slider(
+                minimum=0, maximum=15, step=1, value=0,
+                label="MIDI Channel"
+            )
+
+            semitone_slider = gr.Slider(
+                minimum=-11, maximum=11, step=1, value=0,
+                label="Semitone Offset"
+            )
+
+            octave_slider = gr.Slider(
+                minimum=-8, maximum=8, step=1, value=0,
+                label="Octave Offset"
+            )
+
+            interval_slider = gr.Slider(
+                minimum=1, maximum=16, step=1, value=8,
+                label="Time Interval"
+            )
+
+            run_button = gr.Button("Convert")
+
+        with gr.Column():
+            output_text = gr.Textbox(label="Jianpu Output")
+
+    run_button.click(
+        fn=midi_file_to_jianpu_str,
+        inputs=[
+            midi_input,
+            channel_slider,
+            semitone_slider,
+            octave_slider,
+            interval_slider
+        ],
+        outputs=output_text
+    )
+
 
 if __name__ == "__main__":
-    interface.launch()
+    ui.launch()
